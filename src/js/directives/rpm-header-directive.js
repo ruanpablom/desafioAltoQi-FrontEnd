4c852c3a7302b89bdf678dbaf2bafd7a5903eb12
ruanpablom <pm.ruan@gmail.com>
2017-09-08 12:54:58 -0300

    github pages

--- src/js/directives/rpm-header-directive.js
+++ src/js/directives/rpm-header-directive.js
@@ -1,7 +1,7 @@
 class RpmHeaderDirective {
   constructor(){
     this.restrict = 'E';
-    this.templateUrl = '/views/rpm-header.html';
+    this.templateUrl = './views/rpm-header.html';
     this.scope = {};
     this.controller = 'rpmHeaderController';
     this.controllerAs = 'vm';
--- src/js/directives/rpm-survey-directive.js
+++ src/js/directives/rpm-survey-directive.js
@@ -1,7 +1,7 @@
 class RpmSurveyDirective {
     constructor(){
       this.restrict = 'E';
-      this.templateUrl = '/views/rpm-survey.html';
+      this.templateUrl = './views/rpm-survey.html';
       this.scope = {};
       this.controller = 'rpmSurveyController';
       this.controllerAs = 'vm';
